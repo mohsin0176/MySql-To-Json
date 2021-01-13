fbbaa1509eec4b287788be87a5bfaa8ee18060c3
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-13 21:30:07 +0600

    csv to json

---
+++ csv to json.php
@@ -0,0 +1,24 @@
+<?php
+// php function to convert csv to json format
+function csvToJson($fname) {
+    // open csv file
+    if (!($fp = fopen($fname, 'r'))) {
+        die("Can't open file...");
+    }
+    
+    //read csv headers
+    $key = fgetcsv($fp,"1024",",");
+    
+    // parse csv rows into array
+    $json = array();
+        while ($row = fgetcsv($fp,"1024",",")) {
+        $json[] = array_combine($key, $row);
+    }
+    
+    // release file handle
+    fclose($fp);
+    
+    // encode array to json
+    return json_encode($json);
+}
+?>
